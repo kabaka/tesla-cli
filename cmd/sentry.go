b97e6a4a61d7dab24f461b39c3a78f1a70c167fa
Kyle Johnson <kyle@vacantminded.com>
2019-06-27 04:10:23 +0000

    cmd/sentry: cobra for subcommands instead of args
    The first pass was a quick and dirty series of if/else on args. Since
    this overall seems to work, I've refined this tiny file to properly use
    Cobra for subcommands.
    
    Additionally, support has been added to retrieve Sentry Mode status via
    Vehicle State query.

--- cmd/sentry.go
+++ cmd/sentry.go
@@ -2,7 +2,6 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -12,48 +11,64 @@ var sentryCmd = &cobra.Command{
 	Use:   "sentry status|on|off",
 	Short: "enable or disable Sentry Mode",
 	Long:  `Enable or disable Sentry Mode.`,
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+var sentryOnCmd = &cobra.Command{
+	Use:   "on",
+	Short: "enable Sentry Mode",
+	Long:  `Enable Sentry Mode.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		if len(args) != 1 {
-			fmt.Println("usage: sentry status|on|off")
-			os.Exit(1)
+		vehicle := GetTeslaVehicle()
+		err := vehicle.SentryModeEnable()
+
+		if err != nil {
+			fmt.Printf("Error enabling sentry mode: %s\n", err)
 		}
 
-		if args[0] == "on" {
-			vehicle := GetTeslaVehicle()
-			err := vehicle.SentryModeEnable()
+		fmt.Println("Sentry mode enabled.")
+	},
+}
 
-			if err != nil {
-				fmt.Printf("Error enabling sentry mode: %s\n", err)
-			}
+var sentryOffCmd = &cobra.Command{
+	Use:   "off",
+	Short: "disable Sentry Mode",
+	Long:  `Disable Sentry Mode.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		vehicle := GetTeslaVehicle()
+		err := vehicle.SentryModeDisable()
 
-			fmt.Println("Sentry mode enabled.")
-			return
-
-		} else if args[0] == "off" {
-			vehicle := GetTeslaVehicle()
-			err := vehicle.SentryModeDisable()
-
-			if err != nil {
-				fmt.Printf("Error enabling sentry mode: %s\n", err)
-			}
-
-			fmt.Println("Sentry mode disabled.")
-			return
+		if err != nil {
+			fmt.Printf("Error enabling sentry mode: %s\n", err)
 		}
 
-		fmt.Println("usage: sentry status|on|off")
-		os.Exit(1)
+		fmt.Println("Sentry mode disabled.")
+	},
+}
+
+var sentryStatusCmd = &cobra.Command{
+	Use:   "status",
+	Short: "get Sentry Mode status",
+	Long:  `Determine whether Sentry Mode is currently enabled or disabled.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		vehicle := GetTeslaVehicle()
+		state, err := vehicle.VehicleState()
+
+		if err != nil {
+			fmt.Printf("Error enabling sentry mode: %s\n", err)
+		}
+
+		if state.SentryMode {
+			fmt.Println("Sentry mode is enabled.")
+		} else {
+			fmt.Println("Sentry mode is disabled.")
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(sentryCmd)
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// sentryCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// sentryCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	sentryCmd.AddCommand(sentryOnCmd)
+	sentryCmd.AddCommand(sentryOffCmd)
+	sentryCmd.AddCommand(sentryStatusCmd)
 }
